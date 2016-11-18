42228aad2edba4c9fe254162b56a616f23ecb2b2
Jan Piorek <jan4292@gmail.com>
2016-11-18 20:19:27 +0200

    To implement interface

--- AssemblyLoader/SPLoader.cs
+++ AssemblyLoader/SPLoader.cs
@@ -11,15 +11,13 @@ namespace Karabina.SharePoint.Provisioning
     public class SPLoader : MarshalByRefObject
     {
         private Assembly assembly = null;
-        private IProvisioningTool type = null;
+        private Type type = null;
         private object sharepointObject = null;
 
-        /*
         private MethodInfo applyProvisioningTemplate = null;
         private MethodInfo createProvisioningTemplate = null;
         private MethodInfo openTemplateForEdit = null;
         private MethodInfo saveTemplateForEdit = null;
-        */
 
         public SPLoader() { }
 
@@ -29,14 +27,14 @@ namespace Karabina.SharePoint.Provisioning
             Type[] types = assembly.GetTypes();
             if (types.Length > 0)
             {
-                type = types[0] as IProvisioningTool;
+                type = types[0];
 
-                //applyProvisioningTemplate = type.GetMethod("ApplyProvisioningTemplate");
-                //createProvisioningTemplate = type.GetMethod("CreateProvisioningTemplate");
-                //openTemplateForEdit = type.GetMethod("OpenTemplateForEdit");
-                //saveTemplateForEdit = type.GetMethod("SaveTemplateForEdit");
+                applyProvisioningTemplate = type.GetMethod("ApplyProvisioningTemplate");
+                createProvisioningTemplate = type.GetMethod("CreateProvisioningTemplate");
+                openTemplateForEdit = type.GetMethod("OpenTemplateForEdit");
+                saveTemplateForEdit = type.GetMethod("SaveTemplateForEdit");
 
-                sharepointObject = Activator.CreateInstance(type as Type);
+                sharepointObject = Activator.CreateInstance(type);
 
             }
 
@@ -46,54 +44,41 @@ namespace Karabina.SharePoint.Provisioning
                                               Action<string> writeMessage,
                                               Action<string[]> writeMessageRange)
         {
-            return type.ApplyProvisioningTemplate(provisioningOptions, writeMessage, writeMessageRange);
-
-            /*
             object[] args = new object[] { provisioningOptions, writeMessage, writeMessageRange };
 
             object result = applyProvisioningTemplate.Invoke(sharepointObject, args);
 
             return (bool)result;
-            */
+
         }
 
         public bool CreateProvisioningTemplate(ProvisioningOptions provisioningOptions,
                                                Action<string> writeMessage,
                                                Action<string[]> writeMessageRange)
         {
-            return type.CreateProvisioningTemplate(provisioningOptions, writeMessage, writeMessageRange);
-
-            /*
             object[] args = new object[] { provisioningOptions, writeMessage, writeMessageRange };
 
             object result = createProvisioningTemplate.Invoke(sharepointObject, args);
 
             return (bool)result;
-            */
+
         }
 
         public TemplateItems OpenTemplateForEdit(string templatePath, string templateName)
         {
-            return type.OpenTemplateForEdit(templatePath, templateName);
-
-            /*
             object[] args = new object[] { templatePath, templateName };
 
             object result = openTemplateForEdit.Invoke(sharepointObject, args);
 
             return (TemplateItems)result;
-            */
+
         }
 
         public void SaveTemplateForEdit(TemplateItems templateItems)
         {
-            type.SaveTemplateForEdit(templateItems);
-
-            /*
             object[] args = new object[] { templateItems };
 
             saveTemplateForEdit.Invoke(sharepointObject, args);
-            */
 
         }
 
